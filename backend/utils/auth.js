0aaead6bc7c70ff55fb441ec9c3b5c797ddaa595
chovin <chovin@users.noreply.github.com>
2023-12-01 23:00:05 +1000

    expire hashes more often clean up on every login

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -2,7 +2,27 @@ const bcrypt = require("bcrypt")
 
 const connection_hashes = {}
 
+function expired(hash) {
+    if (connection_hashes[hash]) {
+        return connection_hashes[hash] - (new Date()) <= 0
+    }
+    return true
+}
+
+function expire_key(hash) {
+    delete connection_hashes[hash]
+}
+
+function expire_hashes() {
+    for (let hash of Object.keys(connection_hashes)) {
+        if (expired(hash)) {
+            expire_key(hash)
+        }
+    }
+}
+
 async function hash(username, password, expires_in_days) {
+    expire_hashes()
     const ms = expires_in_days*24*60*60*1000
     const expiry = new Date(new Date().getTime() + ms)
     const hash = await bcrypt.hash(username + '|' + password, 10)
@@ -12,12 +32,11 @@ async function hash(username, password, expires_in_days) {
 
 function authenticate(key) {
     if (connection_hashes[key]) {
-        if (connection_hashes[key] - (new Date()) > 0) {
-            return true
-        } else {
-            delete connection_hashes[key]
+        if (expired(key)) {
+            expire_key(key)
             return false
         }
+        return true
     }
     return false
 }
